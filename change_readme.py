4bbee3a5c813d77e24a0acf0c36fc6ecd00088d7
topogar <alex.berez12345@gmail.com>
2020-05-31 12:41:20 +0300

    update readme

--- change_readme.py
+++ change_readme.py
@@ -1,11 +1,11 @@
 import sys
 import re
 
-repo_name = sys.argv[1]
+github_name = sys.argv[1]
 
 with open('README.md', 'r') as f:
     readme = f.read()
-    readme = re.sub('lola', repo_name, readme)
+    readme = re.sub('topogar', github_name, readme)
 
 with open('README.md', 'w') as f:
     f.write(readme)
